9e0391c75f7fc713784c50d096373a92c4e1fa82
lukassousaf <lukasdsfigueiredo@gmail.com>
2020-11-17 18:30:26 -0300

    second module

---
+++ first-module/second-module/main.js
@@ -0,0 +1,12 @@
+// 1.1
+import { Usuario as ClasseUsuario } from '/functions.js';
+ClasseUsuario.info();
+
+// 1.2
+import { idade } from './functions.js';
+console.log(idade);
+
+// 1.3
+import { idade as idadeUsuario, Usuario } from '/functions.js';
+Usuario.info()
+console.log(idadeUsuario);
